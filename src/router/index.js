ba0c9ef27b9435c0137c5081b1a11392563c47c9
raviejay <puspusraviejay@gmail.com>
2025-08-24 18:45:22 +0800

    add index

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CallBack from '@/components/auth/CallBack.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,7 +12,8 @@ const router = createRouter({
     },
     {
       path: '/auth/callback',
-      redirect: '/',
+      name: 'auth-callback',
+      component: CallBack,
     },
   ],
 })
